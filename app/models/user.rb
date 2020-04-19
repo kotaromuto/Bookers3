b24ec973f65162685c801fbd7ae2762e026a4af4
kotaromuto <kotaromuto19931004@gmail.com>
2020-04-19 22:05:12 +0900

    [Add]いいね機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable,:validatable
 
   has_many :books,dependent: :destroy
-  
+  has_many :favorites,dependent: :destroy#favoriteと関連付け
 
   #バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
   validates :name, length: {maximum: 20, minimum: 2}
